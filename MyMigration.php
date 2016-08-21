a9b1157498f3244e005e3c2572b48da3d9979d1b
corvenik <corvenik@gmail.com>
2016-08-21 22:28:19 +0800

    Push the right version.

--- MyMigration.php
+++ MyMigration.php
@@ -1,31 +1,36 @@
 <?php
 
 /**
- 
- * User: LingFeng
- * Date: 2015/11/16
- * Time: 22:49
+ * User: Ian Jiang
+ * Date: 2016/08/21
  */
+
 class MyMigration
 {
-    protected $CI;
-    var $tables = '*';
-    var $write_file = TRUE;
+    private   $_ci;
+    protected $tables = '*';
+    protected $write_file = TRUE;
     protected $path = '';
-    var $skip_tables = array();
-    var $add_view = false;
+    protected $skip_tables = array();
+    protected $add_view = FALSE;
+    private   $_table_num = 1;
+    private   $_migration_folder_name = 'migrations';
  
     function __construct()
     {
-        // parent::__construct();
-        isset($this->CI) OR $this->CI =& get_instance();
-        $this->path = APPPATH . 'migrations';
-        $this->CI->load->database();
+        // Get Codeigniter Object
+        if(!isset($this->_ci))
+        {
+            $this->_ci =& get_instance();
+        }
 
+        $this->path = APPPATH . $this->_migration_folder_name;
+
+        $this->_ci->load->database();
     }
 
     /**
-     * 生成migrations file
+     * Generate Migrations Files
      *
      * @param string $tables
      * @return boolean|string
@@ -33,52 +38,74 @@ class MyMigration
     function generate($tables = null)
     {
         //准备$this->tables
-        if (null == $tables || '*' == $tables) {
-            $query = $this->CI->db->query('SHOW full TABLES FROM ' . $this->CI->db->protect_identifiers($this->CI->db->database));
+        if (null == $tables || '*' == $tables)
+        {
+            $query = $this->_ci->db->query('SHOW full TABLES FROM ' . $this->_ci->db->protect_identifiers($this->_ci->db->database));
 
             $tmp = array();
-            if ($query->num_rows() > 0) {
-                foreach ($query->result_array() as $row) {
-                    $tablename = 'Tables_in_' . $this->CI->db->database;
-                    if (isset($row[$tablename])) {
+
+            if ($query->num_rows() > 0)
+            {
+                foreach ($query->result_array() as $row)
+                {
+                    $table_name = 'Tables_in_' . $this->_ci->db->database;
+
+                    if (isset($row[$table_name]))
+                    {
+
                         /* check if table in skip arrays, if so, go next */
-                        if (in_array($row[$tablename], $this->skip_tables))
+                        if (in_array($row[$table_name], $this->skip_tables))
+                        {
                             continue;
+                        }
+
                         /* check if views to be migrated */
-                        if ($this->add_view) {
+                        if ($this->add_view)
+                        {
                             ## not implemented ##
-                            //$retval[] = $row[$tablename];
-                        } else {
+                            //$retval[] = $row[$table_name];
+                        }
+                        else
+                        {
                             /* skip views */
                             if (strtolower($row['Table_type']) == 'view') {
                                 continue;
                             }
-                            $tmp[] = $row[$tablename];
+                            $tmp[] = $row[$table_name];
                         }
                     }
                 }
             }
-            for($i=0;$i<count($tmp);$i++){ //去除表前缀
-                $tmp[$i]= str_ireplace($this->CI->db->dbprefix,"",$tmp[$i]); //大小写不敏感
+
+            for($i=0, $cnt = count($tmp); $i < $cnt; $i++)
+            { //去除表前缀
+                $tmp[$i]= str_ireplace($this->_ci->db->dbprefix,"",$tmp[$i]); //大小写不敏感
             }
 
             $this->tables = $tmp;
-        } else {
+        }
+        else
+        {
             $this->tables = is_array($tables) ? $tables : explode(',', $tables);
         }
 
         //循环生成文件
-        foreach ($this->tables as $table) {
+        foreach ($this->tables as $table)
+        {
             unset($migration);
             log_message('debug', print_r($table, true));
             $str = $this->getFileString($table, $this->up($table), $this->down($table));
-            if($this->write_file){
 
-                if (NULL != $str) {
+            if($this->write_file)
+            {
+                if (NULL != $str)
+                {
                     $result = $this->writeFile($table,$str);
                     continue;
                 }
-            }else{
+            }
+            else
+            {
                 //TODU 输出
                 var_dump( $str);
             }
@@ -101,7 +128,8 @@ class MyMigration
     {
         $str = '<?php ';
         $str .= 'defined(\'BASEPATH\') OR exit(\'No direct script access allowed\');' . "\n\n";
-        $str .= "class Migration_create_$table extends CI_Migration {" . "\n";
+        $str .= "class Migration_create_$table extends CI_Migration" . "\n";
+        $str .= '{' . "\n";
         $str .= $up;
         $str .= $down;
         $str .= '}';
@@ -114,7 +142,8 @@ class MyMigration
      * @param $table
      * @param $str
      */
-    function  writeFile($table,$str){
+    function  writeFile($table,$str)
+    {
         $file = $this->openFile($table);
         fwrite($file, $str);
         fclose($file);
@@ -128,8 +157,10 @@ class MyMigration
     function checkPermission($fileName)
     {
         //file permissions
-        if ($this->write_file) {
-            if (!is_dir($this->path) OR !is_really_writable($this->path)) {
+        if ($this->write_file)
+        {
+            if (!is_dir($this->path) OR !is_really_writable($this->path))
+            {
                 $msg = "can not write migration file to " . $this->path;
                 log_message('error', $msg);
                 return FALSE;
@@ -146,16 +177,20 @@ class MyMigration
     function openFile($fileName)
     {
 
-        $file_path = $this->path . '/create_' . $fileName . '.php';
+        $file_path = $this->path . '/' . sprintf('%03d', $this->_table_num) .'_create_' . $fileName . '.php';
         $file = fopen($file_path, 'w+');
-        if (!$file) {
+
+        if ( ! $file)
+        {
             $msg = 'No File';
             log_message('error', $msg);
 //            echo $msg;
             return FALSE;
         }
-        return $file;
 
+        $this->_table_num++;
+
+        return $file;
     }
 
     /**
@@ -165,37 +200,99 @@ class MyMigration
      */
     function up($tableName)
     {
-        $str = "\n\t" . 'public function up() {' . "\n";
+        $str = "\n\t" . '/**' . "\n";
+        $str .= "\t" . '* up (create table)' . "\n";
+        $str .= "\t" . '*/' . "\n";
+
+        $str .= "\t" . 'public function up()' . "\n";
+        $str .= "\t" . '{' . "\n";
         $str .= "\n\t\t" . "// Drop table '$tableName' if it exists";
         $str .= "\n\t\t" . '$this->dbforge->drop_table(\'' . $tableName . '\', TRUE);';
 
 
-        $query = $this->CI->db->query('describe ' . $this->CI->db->database . '.' . $this->CI->db->dbprefix($tableName) );
+        $query = $this->_ci->db->query('describe ' . $this->_ci->db->database . '.' . $this->_ci->db->dbprefix($tableName) );
+
         // 如果没有结果，直接返回
-        if (null == $query->result()) return;
+        if (null == $query->result())
+        {
+            return FALSE;
+        }
 
         $columns = $query->result_array();//获取列数据
-        $query = $this->CI->db->query(' SHOW TABLE STATUS WHERE Name = \'' . $tableName . '\'');
-        $engines = $query->row_array();
-        $str .= "\n\t\t" . '## Create Table ' . $tableName . "\n";
-        foreach ($columns as $column) {
-            $str .= "\t\t" . '$this->dbforge->add_field("' . "`$column[Field]` $column[Type] " . ($column['Null'] == 'NO' ? 'NOT NULL' : 'NULL') .
-                (                    #  timestamp 默认值不需要加引号
-                $column['Default'] ? ' DEFAULT ' . ($column['Type'] == 'timestamp' ? $column['Default'] : '\'' . $column['Default'] . '\'') : ''
-                )
-                . " $column[Extra]\");" . "\n";
-            if ($column['Key'] == 'PRI')
-                $str .= "\t\t" . '$this->dbforge->add_key("' . $column['Field'] . '",true);' . "\n";
-        }
-        $str .= "\t\t" . '$this->dbforge->create_table("' . $tableName . '", TRUE);' . "\n";
-        if (isset($engines['Engine']) and $engines['Engine'])
-            $str .= "\t\t" . '$this->db->query(\'ALTER TABLE  ' . $this->CI->db->protect_identifiers($tableName) . ' ENGINE = ' . $engines['Engine'] . '\');';
-        if (isset($engines['Comment']) and $engines['Comment'])
-            $str .= "\t\t" . '$this->db->query(\'ALTER TABLE  ' . $this->CI->db->protect_identifiers($tableName) . ' COMMENT = ' . $engines['Comment'] . '\');';
 
-        $data = $this->tableData($tableName);
-        $str .=$data;
+        $add_key_str = '';
+
+        $add_field_str = "\t\t" . '$this->dbforge->add_field(array(' . "\n";
+
+        foreach ($columns as $column)
+        {
+            // Field Begin
+
+            // field name
+            $add_field_str .= "\t\t\t'{$column['Field']}' => array(" . "\n";
+
+            preg_match('/^(\w+)\(([\d]+)\)/', $column['Type'], $match);
+
+            if($match === [])
+            {
+                preg_match('/^(\w+)/', $column['Type'], $match);
+            }
+
+            $add_field_str .= "\t\t\t\t'type' => '" . strtoupper($match[1]) . "'," . "\n";
+
+            $add_field_str .= (isset($match[2])) ? "\t\t\t\t'constraint' => '" . strtoupper($match[2]) . "'," . "\n" : '';
+
+            $add_field_str .= (strstr($column['Type'], 'unsigned')) ? "\t\t\t\t'unsigned' => TRUE," . "\n" : '';
+
+            $add_field_str .= ((string) $column['Default'] !== '') ? "\t\t\t\t'default' => '" . $column['Default'] . "'," . "\n" : '';
+
+            $add_field_str .= ($column['Null'] !== 'NO') ? "\t\t\t\t'null' => TRUE," . "\n" : '';
+
+//            die();
+//
+//            $add_field_str .= "\t\t" . '$this->dbforge->add_field("' . "`$column[Field]` $column[Type] " . ($column['Null'] == 'NO' ? 'NOT NULL' : 'NULL');
+//            #  timestamp 默认值不需要加引号
+//            $add_field_str .= ($column['Default'] ? ' DEFAULT ' . ($column['Type'] == 'timestamp' ? $column['Default'] : '\'' . $column['Default'] . '\'') : '') . " $column[Extra]\");" . "\n";
+
+            $add_field_str .= "\t\t\t)," . "\n";
+            // Field End
+
+            if ($column['Key'] == 'PRI')
+            {
+                $add_key_str .= "\n\t\t" . '$this->dbforge->add_key("' . $column['Field'] . '",true);' . "\n";
+            }
+        }
+
+        $add_field_str .= "\t\t));" . "\n";
+
+        $str .= "\n\n\t\t" . '// Add Fields.' . "\n";
+        $str .= $add_field_str;
+
+        $str .= "\n\t\t" . '// Add Primary Key.' . "\n";
+        $str .= $add_key_str;
+
+        // create db
+
+        $query = $this->_ci->db->query(' SHOW TABLE STATUS WHERE Name = \'' . $tableName . '\'');
+
+        $engines = $query->row_array();
+
+        $attributes_str = "\n\t\t" . '$attributes_str = array(' . "\n";;
+        $attributes_str .= ((string) $engines['Engine'] !== '') ? "\t\t\t'ENGINE' => '" . $engines['Engine'] . "'," . "\n" : '';
+        $attributes_str .= ((string) $engines['Comment'] !== '') ? "\t\t\t'COMMENT' => '" . $engines['Comment'] . "'," . "\n" : '';
+        $attributes_str .= "\t\t" . ');' . "\n";
+
+        $str .= "\n\t\t" . '// Table attributes.' . "\n";
+        $str .= $attributes_str;
+
+        $str .= "\n\t\t" . '// Create Table ' . $tableName . "\n";
+        $str .= "\n\t\t" . '$this->dbforge->create_table("' . $tableName . '", TRUE, $attributes_str);' . "\n";
+
+        // date部分暫時不塞
+//        $data = $this->tableData($tableName);
+//        $str .=$data;
         $str .= "\n\t" . ' }' . "\n";
+
         return $str;
     }
 
@@ -206,7 +303,7 @@ class MyMigration
     function tableData($tableName)
     {
         $str = "\n\t\t" . "//GET $tableName data";
-        $query = $this->CI->db->get($tableName);
+        $query = $this->_ci->db->get($tableName);
         $result = $query->result();
         if (null == $result) {
             $str .= "\n\t\t" . "//no data";
@@ -231,10 +328,7 @@ class MyMigration
         $str .="\t\t". "//INSERT bath data" . "\n";
         $str .="\t\t".'$this->db->insert_batch(\''.$tableName.'\',$data);' ."\n";
 
-
-
         return $str;
-
     }
 
     /**
@@ -244,8 +338,13 @@ class MyMigration
      */
     function down($tableName)
     {
-        $str = "\n\t" . 'public function down() {'."\n";
-        $str .= "\t\t" . '### Drop table ' . $tableName . ' ##' . "\n";
+        $str = "\n\t" . '/**' . "\n";
+        $str .= "\t" . '* down (drop table)' . "\n";
+        $str .= "\t" . '*/' . "\n";
+
+        $str .= "\t" . 'public function down()' . "\n";
+        $str .= "\t" . '{' . "\n";
+        $str .= "\t\t" . '// Drop table ' . $tableName . "\n";
         $str .= "\t\t" . '$this->dbforge->drop_table("' . $tableName . '", TRUE);' . "\n";
         $str .= "\t" . '}' . "\n";
         return $str;
